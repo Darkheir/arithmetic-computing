ffe99b1ddcad72d2f8a18bf6c2688977da8f26af
rcohen <raphael.cohen@airbus.com>
2016-08-06 20:48:04 +0200

    Improve error handling, socket is now dependancy injected

--- client.py
+++ client.py
@@ -12,25 +12,22 @@ from arithmetic_computing.helper.file_access import read_lines, write_lines
 
 class Client(object):
 
-    def __init__(self):
+    def __init__(self, client_sock):
         self._operations = None
-        self._calculation_results = None
+        self._calculation_results = []
+        self._client_socket = client_sock
 
-    def calculate_on_server(self, socket):
+    def calculate_on_server(self):
         """Send operations to calculate to the server and retrieve the results
-
-        :param socket: Socket to connect to
-        :type socket: str
         """
-        socket_client = ClientSocket(socket)
+        socket_client = self._client_socket
         try:
             socket_client.open()
             socket_client.send(self._operations)
             self._calculation_results = socket_client.receive()
         except CommunicationError, comm_error:
             logging.error("Communication error: %s", str(comm_error))
-            logging.info("Exiting")
-            sys.exit(1)
+            raise
         finally:
             socket_client.close()
 
@@ -44,8 +41,7 @@ class Client(object):
             self._operations = read_lines(operation_path)
         except IOError, io_error:
             logging.error("Error reading operations: %s", str(io_error))
-            logging.info("Exiting")
-            sys.exit(1)
+            raise
 
     def write_results(self, result_path):
         """Write the operations results into the given file
@@ -57,8 +53,7 @@ class Client(object):
             write_lines(result_path, self._calculation_results)
         except IOError, io_error:
             logging.error("Error writing result: %s", str(io_error))
-            logging.info("Exiting")
-            sys.exit(1)
+            raise
 
 
 if __name__ == '__main__':
@@ -68,9 +63,14 @@ if __name__ == '__main__':
     timer = Timer()
     timer.start()
 
-    client = Client()
-    client.load_operations(operation_file)
-    client.calculate_on_server(socket_address)
-    client.write_results(result_file)
+    client_socket = ClientSocket(socket_address)
+    client = Client(client_socket)
+    try:
+        client.load_operations(operation_file)
+        client.calculate_on_server()
+        client.write_results(result_file)
+    except Exception:
+        logging.info("Exiting")
+        sys.exit(1)
 
     logging.info("Mathematical duration: %f seconds", timer.time())
